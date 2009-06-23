9299a42b0abe7f9d773257a65c4d03d29f83a961
jamtoday <jamesalexanderlevy@gmail.com>
2009-06-23 01:59:22 -0700

    adjusted HTML and JS to meet style guides

--- app/soc/content/js/take_survey.js
+++ app/soc/content/js/take_survey.js
@@ -23,100 +23,106 @@ $(function () {
   *
   */
 
-  var widget = $('div#survey_widget');
-  widget.parents('td.formfieldvalue:first').css({
-    'float': 'left',
-    'width': 200
-  });
-  var survey = widget.find('tbody:first');
+var widget = $('div#survey_widget');
+widget.parents('td.formfieldvalue:first').css({
+'float': 'left',
+'width': 200
+});
+var survey = widget.find('tbody:first');
 
-  if (widget.hasClass('create')) {
+if (widget.hasClass('create')) {
 
-    /*
-    * == Set Custom Field Rules ==
-    *
-    */
-    widget.find('input').each(function () {
-      $(this).preserveDefaultText($(this).val());
-    });
+/*
+* == Set Custom Field Rules ==
+*
+*/
+widget.find('input').each(function () {
+  $(this).preserveDefaultText($(this).val());
+});
 
-    widget.find('textarea').each(function () {
-      $(this)
-      .preserveDefaultText($(this).val()).attr('overflow', 'auto')
-      .growfield();
-    });
+widget.find('textarea').each(function () {
+  $(this)
+  .preserveDefaultText($(this).val()).attr('overflow', 'auto')
+  .growfield();
+});
 
+}
+
+else { // survey has saved results
+widget.find('textarea').each(function () {
+  $(this).attr('overflow', 'auto').growfield();
+}).end()
+.find('.pick_multi').each(function() {
+  $(this).find('input').each(function(){
+   // if $(this).attr('checked', 'true');});
+     });
+});
+  
   }
-
-  else { // survey has saved results
-    widget.find('textarea').each(function () {
-      $(this).attr('overflow', 'auto').growfield();
-    }).end()
-   .find('.pick_multi').each(function() {
-      $(this).find('input').each(function(){
-       // if $(this).attr('checked', 'true');});
-         });
-    });
-      
-      }
-      
+  
 
   /*
   * == Configure Project ==
   *
   */      
+
+// remember if form has been touched
+$('input,textarea,select')
+.change(function(){
+  if ($(this).attr('id') == 'id_project') return;
+  $('form:first').data('touched', true);
+});
+
+// remember initially chosen project choice
+$('select#id_project').blur(function(){
+$(this).data('selected', $(this).find('option:first'));
+}).change(function(){
+  if ($('form:first').data('touched') == true)
+  {
+  // if form has been touched, send confirmation dialog
+  var save_check = confirm(
+  "Switching projects will lose unsaved edits made to this survey."
+  );
+  if (!save_check) {
+    $(this).data('selected').attr('selected', 'selected');
+    return false; }
+  }
+  if ($(this).val() != 'None')
+  // redirect with new project GET param
+  window.location=window.location.href.split('?')[0] 
+  + "?project=" + $(this).val();
+});
   
-  $('input,textarea,select')
-  .change(function(){
-    if ($(this).attr('id') == 'id_project') return;
-    $('form:first').data('touched', true);
-  });
-  
-  $('select#id_project').blur(function(){
-   $(this).data('selected', $(this).find('option:first'));
-  }).change(function(){
-    if ($('form:first').data('touched') == true)
-    {
-    var save_check = confirm(
-    "Switching projects will lose unsaved edits made to this survey."
-    );
-    if (!save_check) {
-      $(this).data('selected').attr('selected', 'selected');
-      return false; }
-    }
-    if ($(this).val() != 'None')
-    window.location=window.location.href.split('?')[0] + "?project=" + $(this).val();
-  });
-      
-   $('div#project_link').insertAfter($('select#id_project')).show();
-     
-  /*
-  * == Survey Submission Handler ==
-  *
-  */
+// insert project link after project select field
+$('div#project_link').insertAfter($('select#id_project')).show();
+ 
+/*
+* == Survey Submission Handler ==
+*
+*/
 
 // validate form 
-  $('input[type=submit]').bind('click', function(e) {
-    e.preventDefault(); 
-    // validate project and grade choice fields
-    if ($('select#id_project') && 
-    $('select#id_project').val() == 'None')
-    return alert('Please Choose a Project');
-    if ($('select#id_grade') && 
-    $('select#id_grade').val() == 'None')
-    return alert('Please Choose a Grade');
-   
-    $('form').trigger('submit');
-    
-  });
+$('input[type=submit]').bind('click', function(e) {
+  e.preventDefault(); 
+  // validate project and grade choice fields
+  if ($('select#id_project') && 
+  $('select#id_project').val() == 'None')
+  return alert('Please Choose a Project');
+  if ($('select#id_grade') && 
+  $('select#id_grade').val() == 'None')
+  return alert('Please Choose a Grade');
+ 
+  $('form').trigger('submit');
   
+});
 
-    
-    
-  $('form').bind('submit', function() {
 
-    $('input#id_s_html').val(
-    widget.find('div#survey_options').remove().end().html()
-    );
-  });
+  
+  
+$('form').bind('submit', function() {
+
+  $('input#id_s_html').val(
+  widget.find('div#survey_options').remove().end().html()
+  );
+});
 });
